f61b6944b1fc0ca03acd0183a6388f2630c0749d
Alex Petrov <i@petrov.com.ru>
2019-01-15 01:12:08 +0300

    more on when to use destructuring

---
+++ destructuring/points.js
@@ -0,0 +1,12 @@
+const points = [
+    [4, 5],
+    [10, 1],
+    [0, 40]
+];
+
+const newPoints = points.map(( [x, y] ) => {
+    // return {x: x, y: y}
+    return {x, y}
+});
+
+console.log(newPoints);
