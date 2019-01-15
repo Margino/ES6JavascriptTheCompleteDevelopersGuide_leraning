b807001deba8eb33834a067f7874f721922f9076
Alex Petrov <i@petrov.com.ru>
2019-01-15 10:47:40 +0300

    prototypal inheritance

--- class/cars.js
+++ class/cars.js
@@ -10,5 +10,24 @@ const car = new Car({
     title: 'Ford'
 });
 
-console.log(car);
-console.log(car.drive());
+console.log('Car:', car);
+console.log('Car.drive:', car.drive());
+
+const Toyota = function(options) {
+    Car.call(this, options);
+    this.color = options.color;
+};
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+Toyota.prototype.honk = function() {
+    return 'beep';
+}
+
+const carT = new Toyota({
+    color: 'red',
+    title: 'Daily Driver'
+});
+
+console.log('Toyota:', carT);
+console.log('Toyota.drive:', carT.drive());
+console.log('Toyota.honk:', carT.honk());
