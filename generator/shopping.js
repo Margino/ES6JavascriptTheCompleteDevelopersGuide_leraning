40c8c922b171d4c7e3062aa600d1d7ada579b44e
Alex Petrov <i@petrov.com.ru>
2019-01-15 22:34:11 +0300

    another step in our generator story

--- generator/shopping.js
+++ generator/shopping.js
@@ -6,11 +6,15 @@ function* shopping() {
     // go into the store with cash
     const staffFromStore = yield 'cash';
 
+    // walking to laundry place
+    const cleanClothes = yield 'laundry';
+
     // walking back home
-    return staffFromStore;
+    return [staffFromStore, cleanClothes];
 }
 
 // stuff in the store
 const gen = shopping();
 console.log(gen.next()); // living our house
 console.log(gen.next('groceries')); // living the store with groceries
+console.log(gen.next('clean clothes'));
